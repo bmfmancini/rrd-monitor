353db16cc2269a08b59dbe82ae71fb33948c3dad
bmfmancini <sean@seanmancini.com>
2019-05-01 13:11:24 -0400

    better layout

--- rrd-monitor.sh
+++ rrd-monitor.sh
@@ -1,21 +1,18 @@
 ##Author Sean Mancini
 ##Purpose This script will monitor a RRD  file to ensure it is being updated to ensure graphing is working
-
+#Version 1.0
 
 #/bin/bash
 
-
+##User settings
 path_to_rrd="i.e /var/www/html/cacti/rra/xyzhost.rrd"
 logging_file=" i.e /var/log/mylog.txt"
 email_address="" #future 
 
-##Get details from rrdtool
 
+##Get details from rrdtool
 rrd_update="$(rrdtool lastupdate   $path_to_rrd  | awk '{print$1}'   | grep -o '[0-9]\+')"
 rrd_lastupdate=$rrd_update
-
-
-
 if [ $rrd_update == $rrd_lastupdate ]
  then 
   i=0
@@ -33,7 +30,7 @@ echo "Graphs are updating"   $('date')  >> $logging_file
 fi
 
 
-echo $i
+
 
 
 
