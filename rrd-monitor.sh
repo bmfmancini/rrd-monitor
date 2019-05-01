827f18919d59f1bbc863c9bc45342d39b75391a0
bmfmancini <sean@seanmancini.com>
2019-05-01 13:41:00 -0400

    Added email logic ##Requires SSMTP but can use sendmail or whatever

--- rrd-monitor.sh
+++ rrd-monitor.sh
@@ -25,6 +25,7 @@ else
 if [ $i == 1 ]
  then
 echo "Graphs are not updating"   $('date')  >> $logging_file
+echo "RRD GRAPHS ARE NOT UPDATING!!!" | ssmtp $email_address
  else 
 echo "Graphs are updating"   $('date')  >> $logging_file
 fi
