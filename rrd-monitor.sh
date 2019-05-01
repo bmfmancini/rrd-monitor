4d104e507b6ffc55fb32cf64840bb209a2d3c374
bmfmancini <sean@seanmancini.com>
2019-05-01 12:55:25 -0400

    Fixed Logging

--- rrd-monitor.sh
+++ rrd-monitor.sh
@@ -5,23 +5,30 @@
 #/bin/bash
 
 
+
+
+
 ##Get details from rrdtool
 
 rrd_update="$(rrdtool lastupdate   /var/www/html/cacti/rra/local_linux_machine_loss_101.rrd  | awk '{print$1}'   | grep -o '[0-9]\+')"
 rrd_lastupdate=$rrd_update
 
-if [ $rrd_update == $rrd_lastupdate ]
-then
-i=0
-else 
-i=1
-fi
 
-if [ $i == 1 ]
-then
-echo "Graphs are not updating!!"
+
+if [ $rrd_update == $rrd_lastupdate ]
+ then 
+  i=0
 else 
-echo "everthing seems to be fine"
+ i=1
+  fi
+
+
+##Logging
+if [ $i == 1 ]
+ then
+echo "Graphs are not updating"   $('date')  >> /var/log/rrd-monitor.log
+ else 
+echo "Graphs are updating"   $('date')  >> /var/log/rrd-monitor.log
 fi
 
 
