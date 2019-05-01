0979bfc969830f2eae3070f90ce2b63e2f2bd36a
bmfmancini <sean@seanmancini.com>
2019-05-01 15:34:09 -0400

    Edited formatting and added lines for cacti specific logs

--- rrd-monitor.sh
+++ rrd-monitor.sh
@@ -6,29 +6,32 @@
 
 ##User settings
 path_to_rrd="i.e /var/www/html/cacti/rra/xyzhost.rrd"
-logging_file=" i.e /var/log/mylog.txt"
+logging_file=" i.e /var/www/html/cacti/log/cacti.log"
 email_address="" #future 
 
 
 ##Get details from rrdtool
 rrd_update="$(rrdtool lastupdate   $path_to_rrd  | awk '{print$1}'   | grep -o '[0-9]\+')"
-rrd_lastupdate=$rrd_update
-if [ $rrd_update == $rrd_lastupdate ]
- then 
-  i=0
-else 
- i=1
-  fi
+ rrd_lastupdate=$rrd_update
+   if [ $rrd_update == $rrd_lastupdate ]
+    then 
+      i=0
+       else 
+        i=1
+          fi
 
 
-##Logging
+##Logging messeges
 if [ $i == 1 ]
  then
 echo "Graphs are not updating"   $('date')  >> $logging_file
+#"WARNING:Graphs are not updating"   $('date')  >> $logging_file ##Uncomment this line to showup nicely in cacti log
 echo "RRD GRAPHS ARE NOT UPDATING!!!" | ssmtp $email_address
  else 
 echo "Graphs are updating"   $('date')  >> $logging_file
+#echo "SYSTEM STATS:Graphs are updating"   $('date')  >> $logging_file ##Uncomment this line to showup nicely in cacti log
 fi
+
 
 
 
