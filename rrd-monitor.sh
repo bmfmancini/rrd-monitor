e5650538461b90751bc24e40f1e0e730f9016cbc
bmfmancini <sean@seanmancini.com>
2019-05-07 10:58:45 -0400

    added higher sleep count for RRD comparison

--- rrd-monitor.sh
+++ rrd-monitor.sh
@@ -10,17 +10,18 @@
 path_to_rrd="/var/www/html/cacti/rra/local_linux_machine_loss_101.rrd"
 logging_file="/var/www/html/cacti/log/cacti.log"
 enable_email="" # enter Y to enable
-email_address="" 
+email_address="sean.mancini@rci.rogers.com" 
 
 
 ##Get details from rrdtool
 rrd_update="$(rrdtool lastupdate   $path_to_rrd  | awk '{print$1}'   | grep -o '[0-9]\+')"
-sleep 5
+sleep 30
 rrd_lastupdate="$(rrdtool lastupdate   $path_to_rrd  | awk '{print$1}'   | grep -o '[0-9]\+')"
 
    if [[ "$rrd_update" = "$rrd_lastupdate" ]]
     then 
       i=1
+     echo "equal"
        else 
         i=0
           fi
@@ -39,10 +40,9 @@ fi
 ###Email alarm to admin (requires ssmtp)
 
 if [ $i == 1 ] && [ $enable_email == "y" ]
-then (echo "Subject: !!!Cacti Graphing Alert!!!"; echo "From: "email" "; echo "To: Me <$email_address>"; echo ""; echo "Graphs are not updating") | ssmtp -vv $email_address
+then (echo "Subject: !!!Cacti Graphing Alert!!!"; echo "From: "email" "; echo "To: Me <$email_address>"; echo ""; echo "Graphs are not updating") | /usr/sbin/ssmtp  -vv $email_address
 
 
-echo test
 
 fi
 
